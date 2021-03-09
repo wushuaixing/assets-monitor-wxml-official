ac0d9122f27761eeae93866756a76f6664c31ee5
zhanglulu <zhangll@yczcjk.com>
2021-03-09 11:30:42 +0800

    -fix [ID1011005]  初始化不用显示无业务缺省页 + 无资产线索缺省页

--- src/models/home/index.ts
+++ src/models/home/index.ts
@@ -1,5 +1,5 @@
 import { currentOrganizationApi, assetApi, riskApi } from '../../services/home';
-import { isRule, filterArray, handleDealAuthRule } from '../../utils/tools/common';
+import { handleDealAuthRule } from '../../utils/tools/common';
 import { getAuthRuleUrl } from "../../services/login";
 import { setGlobalData } from "../../utils/const/global";
 
@@ -27,29 +27,11 @@ export default {
     *getAssets({ payload }, {all, call, put }) {
       const res = yield call(assetApi, payload);
       return res;
-      // if(res.code === 200){
-      //   yield put({ type: 'updateAssets', payload: {
-      //       auctionCount: res.data.auctionCount || 0,
-      //       subrogationCount: res.data.subrogationCount || 0,
-      //     }});
-      //   yield put({ type: 'updateAssetsStarLevel', payload: {
-      //       starLevelCounts: res.data.starLevelCounts
-      //     }});
-      // }
     },
 
     *getRisk({ payload}, {all, call, put }) {
       const res = yield call(riskApi, payload);
       return res;
-      // if(res.code === 200){
-      //   yield put({ type: 'updateRisk', payload: {
-      //       bankruptcyCount: res.data.bankruptcyCount || 0,
-      //       lawsuitCount: res.data.lawsuitCount || 0,
-      //     }});
-      //   yield put({ type: 'updateRiskStarLevel', payload: {
-      //       starLevelCounts: res.data.starLevelCounts,
-      //     }});
-      // }
     },
   },
   reducers: {
@@ -59,81 +41,6 @@ export default {
         ...payload,
       }
     },
-
-    // updateAssets(state, { payload }) {
-    //   let newAssetsArrary =  [
-    //     { id: 1, name: '资产拍卖', num: 0, isRule: false, icon: 'icon-auction', value: 'zcwjzcpm'},
-    //     { id: 2, name: '代位权', num: 0, isRule: false, icon: 'icon-subrogation', value: 'zcwjdwq'},
-    //   ];
-    //   newAssetsArrary[0].num = payload.auctionCount || 0;
-    //   newAssetsArrary[0].isRule = isRule('zcwjzcpm');
-    //   newAssetsArrary[1].num = payload.subrogationCount || 0;
-    //   newAssetsArrary[1].isRule = isRule('zcwjdwq');
-    //   return {
-    //     ...state,
-    //     assetsArray: filterArray([...newAssetsArrary]),
-    //   }
-    // },
-    //
-    // updateRisk(state, { payload }) {
-    //   let newRiskArrary = [
-    //     { id: 21, name: '破产重整', num: 0, isRule: false, icon: 'icon-bankruptcy', value: 'fxjkqypccz'},
-    //     { id: 22, name: '涉诉', num: 0, isRule: false, icon: 'icon-litigation', value: 'fxjkssjk'},
-    //   ];
-    //   newRiskArrary[0].num = payload.bankruptcyCount || 0 ;
-    //   newRiskArrary[0].isRule = isRule('fxjkqypccz');
-    //   newRiskArrary[1].num = payload.lawsuitCount || 0;
-    //   newRiskArrary[1].isRule = isRule('fxjkssjk');
-    //   return {
-    //     ...state,
-    //     riskArray: filterArray([...newRiskArrary]),
-    //   }
-    // },
-    //
-    // updateAssetsStarLevel(state, {payload}: { payload: any }): any {
-    //   let assetsStar = [...state.assetsStarLevelCounts];
-    //   if(payload.starLevelCounts.length > 0){
-    //     payload.starLevelCounts.forEach(item => {
-    //       if(item.starLevel === 90){
-    //         assetsStar[0].starLevelCount = item.starLevelCount;
-    //       }
-    //       else if(item.starLevel === 80){
-    //         assetsStar[1].starLevelCount = item.starLevelCount;
-    //       }
-    //       else if(item.starLevel === 60){
-    //         assetsStar[2].starLevelCount = item.starLevelCount;
-    //       }
-    //     });
-    //   }
-    //   return {
-    //     ...state,
-    //     assetsStarLevelCounts: [...assetsStar],
-    //   }
-    // },
-    //
-    // updateRiskStarLevel(state, {payload}: { payload: any }): any {
-    //   let riskStar = [...state.riskStarLevelCounts];
-    //   if(payload.starLevelCounts.length > 0){
-    //     payload.starLevelCounts.forEach(item => {
-    //       if(item.starLevel === 90){
-    //         riskStar[0].starLevelCount = item.starLevelCount;
-    //       }
-    //       else if(item.starLevel === 80){
-    //         riskStar[1].starLevelCount = item.starLevelCount;
-    //       }
-    //       else if(item.starLevel === 60){
-    //         riskStar[2].starLevelCount = item.starLevelCount;
-    //       }
-    //       else{
-    //         riskStar[3].starLevelCount = item.starLevelCount;
-    //       }
-    //     });
-    //   }
-    //   return {
-    //     ...state,
-    //     riskStarLevelCounts: [...riskStar],
-    //   }
-    // },
 
     updateMonitorParams(state, { payload }) {
       let newParmas = {...state.monitorParams, ...payload.params};
